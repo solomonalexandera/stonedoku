b719d25736d7f2651efc7275ac0a96a25d0efd42
solomonalexandera <edmund_alexander@icloud.com>
2025-12-20 12:49:16 +0000

    Fix lint: replace tabs with spaces in functions/index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -27,6 +27,6 @@ import * as logger from "firebase-functions/logger";
 setGlobalOptions({maxInstances: 10});
 
 export const helloWorld = onRequest((request, response) => {
-	logger.info("Hello logs!", {structuredData: true});
-	response.send("Hello from Firebase!");
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
 });
