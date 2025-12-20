554d1c88a57b42a79b44eeac580a461c40711466
solomonalexandera <edmund_alexander@icloud.com>
2025-12-20 12:48:35 +0000

    Add helloWorld function to functions src

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,3 +25,8 @@ import * as logger from "firebase-functions/logger";
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
 setGlobalOptions({maxInstances: 10});
+
+export const helloWorld = onRequest((request, response) => {
+	logger.info("Hello logs!", {structuredData: true});
+	response.send("Hello from Firebase!");
+});
